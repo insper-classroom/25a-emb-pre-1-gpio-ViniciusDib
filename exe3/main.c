0f5f40343012ed6b6e9fc0d8ef81c2bd010b6493
Vinicius Dib <viniciusdib15@gmail.com>
2025-02-14 12:21:01 -0500

    ultima vez

--- exe3/main.c
+++ exe3/main.c
@@ -11,6 +11,7 @@ const int LED_PIN_G = 6;
 int main() {
   stdio_init_all();
 
+
   gpio_init(LED_PIN_R);
   gpio_set_dir(LED_PIN_R, GPIO_OUT);
   gpio_init(LED_PIN_G);
@@ -24,29 +25,31 @@ int main() {
   gpio_init(BTN_PIN2);
   gpio_set_dir(BTN_PIN2, GPIO_IN);
   gpio_pull_up(BTN_PIN2);
-
+  
   bool g = false;
   bool r = false;
 
   while (true) {
     if (!gpio_get(BTN_PIN)){
       sleep_ms(100);
-      while (!gpio_get(BTN_PIN))
-      {
-        r = !r;
-        gpio_put(LED_PIN_R,r);
+      while (!gpio_get(BTN_PIN)){
+      r = !r;
+      gpio_put(LED_PIN_R,r);
+      sleep_ms(100);   
       }
     }
 
     if (!gpio_get(BTN_PIN2)){
         sleep_ms(100);
-        while (!gpio_get(BTN_PIN2))
-        {
-          g = !g;
-          gpio_put(LED_PIN_G,g);
+        while (!gpio_get(BTN_PIN2)){
+        g = !g;
+        gpio_put(LED_PIN_G,g);
+        sleep_ms(100);
         }
     }
+  sleep_ms(10);   
   }  
+
 }
 
 
